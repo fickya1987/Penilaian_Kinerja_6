86a93f2c77a123ad650bfcf08f6d76cd405ee323
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2025-06-04 09:42:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 # Load data
 @st.cache_data
 def load_data():
-    return pd.read_csv("Penilaian_Kinerja_03.csv")
+    return pd.read_csv("Penilaian_Kinerja.csv")
 
 df = load_data()
 
